310c18cb1c12ef0ee53595691938d8ee0469fe29
gobal12 <iiqmuhammad.im@gmail.com>
2025-06-13 14:10:10 +0700

    "main"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class DummyUserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run()
     {
